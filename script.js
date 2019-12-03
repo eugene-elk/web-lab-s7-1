26889b4dd01ebcdb032659b18d69c2c4ba4ed834
Evgeny Lositsky <elosyash239@gmail.com>
2019-12-03 23:21:32 +0300

    added prevent default in event listener

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = function () {
     let form = document.getElementById("form");
 
     form.addEventListener("submit", function (event) {
+        event.preventDefault();
         console.log("button pressed");
     });
 };
