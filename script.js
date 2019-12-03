a6589bc8a7a33d90fb1c942d3bb841438a8a9a1b
Evgeny Lositsky <elosyash239@gmail.com>
2019-12-03 23:15:40 +0300

    added event listener

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    let form = document.getElementById("form");
+
+    form.addEventListener("submit", function (event) {
+        console.log("button pressed");
+    });
+};
+
