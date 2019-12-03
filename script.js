fb17892b13f31efaccb2e2d328ad13fbca744f00
Evgeny Lositsky <elosyash239@gmail.com>
2019-12-04 00:38:45 +0300

    added makeRequest

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ window.onload = function () {
     form.addEventListener("submit", function (event) {
         event.preventDefault();
         console.log("button pressed");
+
+        let city = document.getElementById('cityName').value;
+        let result = makeRequest(city);
+        let result_parsed = JSON.parse(result.responseText);
+        
+        console.log(result_parsed);
     });
 };
 
+function makeRequest(city) {
+    let request = new XMLHttpRequest();
+    let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=1aec2ec35d3525ece44c6dbd16c33b0e';
+    request.open('GET', url, false);
+    request.send();
+
+    return request;
+}
